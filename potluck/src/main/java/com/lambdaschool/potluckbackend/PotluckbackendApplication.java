1f0a8777cdbebb00a258332f81a546776a5ad247
Robert-Rodriguez <robertmasters@hotmail.com>
2021-01-04 17:01:09 -0800

    set up server, not sure if im comfortable setting up the models

---
+++ potluck/src/main/java/com/lambdaschool/potluckbackend/PotluckbackendApplication.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.potluckbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotluckbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PotluckbackendApplication.class, args);
+    }
+
+}
---
+++ potluck/src/test/java/com/lambdaschool/potluckbackend/PotluckbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.potluckbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotluckbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
